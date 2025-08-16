cb44a7553645c768c50813c3469d207e0a47227c
mihtarr <42549719+mihtarr@users.noreply.github.com>
2025-08-17 01:23:30 +0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using MiniBackend.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Services
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=app.db"));
@@ -11,14 +9,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// DB auto create
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Remove HttpsRedirection for Render
+// app.UseHttpsRedirection();
+
 app.UseAuthorization();
 app.MapControllers();
+
+// Render port
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 app.Run();
