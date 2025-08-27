bab4561baa2aff667a022571c2330f8db2ba93af
mihtarr <42549719+mihtarr@users.noreply.github.com>
2025-08-27 21:31:28 +0300

    intial

--- Services/AuthHelper.cs
+++ Services/AuthHelper.cs
@@ -55,7 +55,9 @@ namespace MiniBackend.Services
                     IssuerSigningKey = new SymmetricSecurityKey(key),
                     ValidateIssuer = false,
                     ValidateAudience = false,
-                    ClockSkew = TimeSpan.Zero
+                     RequireExpirationTime = false,
+                     ValidateLifetime = false
+                    //ClockSkew = TimeSpan.Zero
                 }, out SecurityToken validatedToken);
 
                 var jwtToken = (JwtSecurityToken)validatedToken;
