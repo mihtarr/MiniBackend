8962a6f298a69b02ecaa162c1d0f770d885544f8
mihtarr <42549719+mihtarr@users.noreply.github.com>
2025-08-27 20:14:51 +0300

    intial

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,5 +8,4 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
-    public DbSet<Session> Sessions { get; set; }
 }
