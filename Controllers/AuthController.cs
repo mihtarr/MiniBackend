59c39abb734f3427ada68f4212dae98a30f47f36
mihtarr <42549719+mihtarr@users.noreply.github.com>
2025-08-27 20:34:43 +0300

    intial

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -148,7 +148,7 @@ namespace MiniBackend.Controllers
                 return Unauthorized("Missing token.");
 
             // Token ile kullanıcıyı al
-            var user = await authHelper.GetUserFromToken(token);
+            var user = await _authHelper.GetUserFromToken(token);
 
             if (user == null)
                 return Unauthorized("Invalid or expired token.");
@@ -199,7 +199,7 @@ namespace MiniBackend.Controllers
             if (string.IsNullOrEmpty(token))
                 return Unauthorized("Missing token.");
 
-            var user = await authHelper.GetUserFromToken(token);
+            var user = await _authHelper.GetUserFromToken(token);
 
             if (user == null)
                 return Unauthorized("Invalid or expired token.");
